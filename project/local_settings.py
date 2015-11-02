d16700958af68fd72209130643f381e655924dc5
Justin Hammer <justhamr@gmail.com>
2015-11-02 16:48:23 -0700

    update gitignore

--- project/local_settings.py
+++ project/local_settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_&q__7r&-%q*n5cm&#j0_q5eghrv9m86n3h)y0sr9mqbv^5%w8'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
